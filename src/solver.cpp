8182e8cf1b32177f5052e599cb8a3dbd5f8743a0
cd-project <19021229@vnu.edu.vn>
2023-08-16 15:26:10 +0700

    fix

--- src/solver.cpp
+++ src/solver.cpp
@@ -21,108 +21,41 @@ tuple<int, int, double, bool, double> Solver::TwoIndexFlow(double tiLim) {
         std::map<std::string, bool> stringToBoolMap;
         // Variable x (phi) and constraints 5 and 6.
         auto **x = new GRBVar *[n];
-//        for (int i = 0; i < n; i++) {
-//            x[i] = reinterpret_cast<GRBVar *>(new GRBVar *[n]);
-//            for (int j = 0; j < n; j++) {
-//                if (i != j) {
-//                    x[i][j] = model.addVar(0,2, 0.0, GRB_INTEGER, "x_" + to_string(i) + "_" + to_string(j));
-//                }
-//            }
-//        }
-
-    for (int nodeIndex = 0; nodeIndex < nodes.size(); nodeIndex++) {
-        x[nodes[nodeIndex].Index] = reinterpret_cast<GRBVar *>(new GRBVar *[n]);
-        for (int nwdIndex = 0; nwdIndex < nwd.size(); nwdIndex++) {
-            auto i = nodes[nodeIndex].Index;
-            auto j = nwd[nwdIndex].Index;
-            if (i != j) {
-                if (nodes[nodeIndex].Demand == 0) {
-                    x[i][j] = model.addVar(0, 2, 0.0, GRB_INTEGER, "x_" + to_string(i) + "_" + to_string(j));
-                    cout << "added " << "x_" + to_string(i) + "_" + to_string(j) << " as = (0, 1, 2)" << endl;
-                    stringToBoolMap["x_" + to_string(i) + "_" + to_string(j)] = true;
-                } else {
-                    x[i][j] = model.addVar(0, 1, 0.0, GRB_INTEGER, "x_" + to_string(i) + "_" + to_string(j));
-                    stringToBoolMap["x_" + to_string(i) + "_" + to_string(j)] = true;
-                    cout << "added " << "x_" + to_string(i) + "_" + to_string(j) << " as = (0, 1)" << endl;
-                }
-            }
-
-        }
-    }
-
-    for (const auto& pair : stringToBoolMap) {
-        std::cout << "Key: " << pair.first << ", Value: " << pair.second << std::endl;
-    }
-
-    cout << "here222?" << endl;
-    for (int subIndex = 0; subIndex < nss.size(); subIndex++) {
-        auto subsetNodes = nss[subIndex];
-        GRBLinExpr sum;
-        for (int iIdx = 0; iIdx < subsetNodes.Nodes.size(); iIdx++) {
-            auto i = subsetNodes.Nodes[iIdx].Index;
-            for (int jIdx = 0; jIdx < subsetNodes.Nodes.size(); jIdx++) {
-                auto j = subsetNodes.Nodes[jIdx].Index;
-                if (i < j) {
-                    sum += x[i][j];
+        for (int nodeIndex = 0; nodeIndex < nodes.size(); nodeIndex++) {
+            x[nodes[nodeIndex].Index] = reinterpret_cast<GRBVar *>(new GRBVar *[n]);
+            for (int nwdIndex = 0; nwdIndex < nwd.size(); nwdIndex++) {
+                auto i = nodes[nodeIndex].Index;
+                auto j = nwd[nwdIndex].Index;
+                if (i != j) {
+                    if (nodes[nodeIndex].Demand == 0) {
+                        x[i][j] = model.addVar(0, 1, 0.0, GRB_INTEGER, "x_" + to_string(i) + "_" + to_string(j));
+                        cout << "added " << "x_" + to_string(i) + "_" + to_string(j) << " as = (0, 1, 2)" << endl;
+                        stringToBoolMap["x_" + to_string(i) + "_" + to_string(j)] = true;
+                    } else {
+                        x[i][j] = model.addVar(0, 1, 0.0, GRB_INTEGER, "x_" + to_string(i) + "_" + to_string(j));
+                        stringToBoolMap["x_" + to_string(i) + "_" + to_string(j)] = true;
+                        cout << "added " << "x_" + to_string(i) + "_" + to_string(j) << " as = (0, 1)" << endl;
+                    }
                 }
             }
         }
-        model.addConstr(sum, GRB_LESS_EQUAL, subsetNodes.Nodes.size() - subsetNodes.R);
-    }
 
-    // Constraint 2
-    for (int idx = 0; idx < nwd.size(); idx++) {
-        // for all i in V': node without depots
-        int i = nwd[idx].Index;
-        GRBLinExpr sumExpr1;
-        GRBLinExpr sumExpr2;
-        for (int j = 0; j < n; j++) {
-            if (i < j) {
-                sumExpr1 += x[i][j];
+        for (int subIndex = 0; subIndex < nss.size(); subIndex++) {
+            auto subsetNodes = nss[subIndex];
+            GRBLinExpr sum;
+            for (int iIdx = 0; iIdx < subsetNodes.Nodes.size(); iIdx++) {
+                auto i = subsetNodes.Nodes[iIdx].Index;
+                for (int jIdx = 0; jIdx < subsetNodes.Nodes.size(); jIdx++) {
+                    auto j = subsetNodes.Nodes[jIdx].Index;
+                    if (i < j) {
+                        sum += x[i][j];
+                    }
+                }
             }
-            if (i > j) {
-                sumExpr2 += x[j][i];
-            }
+            model.addConstr(sum, GRB_LESS_EQUAL, subsetNodes.Nodes.size() - subsetNodes.R);
         }
-        model.addConstr(sumExpr1 + sumExpr2, GRB_EQUAL, 2);
-        sumExpr1 = 0;
-        sumExpr2 = 0;
-    }
 
-    cout << "here333?" << endl;
-
-//    // Constraint 3
-//    for (int subIndex = 0; subIndex < nss.size(); subIndex++) {
-//        GRBLinExpr sumExpr1;
-//        GRBLinExpr sumExpr2;
-//        for (int idx = 0; idx < nss[subIndex].Nodes.size(); idx++) {
-//            auto i = nss[subIndex].Nodes[idx].Index;
-//            for (int csIdx = 0; csIdx < nss[subIndex].ComplementSet.size(); csIdx++) {
-//                auto j = nss[subIndex].ComplementSet[csIdx].Index;
-//                if (i < j) {
-//                    sumExpr1 += x[i][j];
-//                }
-//            }
-//        }
-//
-//        for (int csIdx = 0; csIdx < nss[subIndex].ComplementSet.size(); csIdx++) {
-//            auto i = nss[subIndex].ComplementSet[csIdx].Index;
-//            for (int idx = 0; idx < nss[subIndex].Nodes.size(); idx++) {
-//                auto j = nss[subIndex].Nodes[idx].Index;
-//                if (i < j) {
-//                    sumExpr2 += x[i][j];
-//                }
-//            }
-//        }
-//
-//        model.addConstr(sumExpr1 + sumExpr2, GRB_GREATER_EQUAL, 2 * nss[subIndex].R);
-//        sumExpr1 = 0;
-//        sumExpr2 = 0;
-//    }
-
-        cout << "444?" << endl;
-
-//      Constraint 4
+        // Constraint 4
         GRBLinExpr constr4;
         string cstr4 = "cstr4";
         for (int idx = 0; idx < nwd.size(); idx++) {
@@ -170,79 +103,79 @@ tuple<int, int, double, bool, double> Solver::TwoIndexFlow(double tiLim) {
 
 }
 
-//tuple<int, int, double, bool, double> Solver::MulticommodityFlow(double tiLim) {
-//    tuple<int, int, double, bool, double> result;
-//    GRBEnv env;
-//    GRBModel model(env);
-//    auto nodes = instance.Nodes;
-//    auto c = instance.DistanceMatrix;
-//    auto n = instance.Dimension;
-//    auto M = instance.NumberOfVehicles; // number of vehicles.
-//    auto nss = instance.NodeSubsets; // subsets of nodes; contains at least 2 nodes; do not contain depots.
-//    auto nwd = instance.NodesWithoutDepots;
-//
-//    auto** x = new GRBVar * [n];
-//    for (int i = 0; i < n; i++) {
-//        x[i] = reinterpret_cast<GRBVar *>(new GRBVar *[n]);
-//        for (int j = 0; j < n; j++) {
-//            x[i][j] = model.addVar(0, 1, 0.0, GRB_BINARY);
-//        }
-//    }
-//
-//    auto*** y = new GRBVar ** [n];
-//    for (int i = 0; i < n; i++) {
-//        y[i] = reinterpret_cast<GRBVar **>(new GRBVar ** [nwd.size()]);
-//        for (int j = 0; j < nwd.size(); j++) {
-//            y[i][j] = reinterpret_cast<GRBVar *>(new GRBVar * [nwd.size()]);
-//            for (int l = 0; l < nwd.size(); l++) {
-//
-//            }
-//        }
-//    }
-//    // Constraint 9
-//    for (int jIdx = 0; jIdx < nwd.size(); jIdx++) {
-//        GRBLinExpr expr;
-//        auto j = nwd[jIdx].Index;
-//        for (int iIdx = 0; iIdx < n; iIdx++) {
-//            auto i = nodes[iIdx].Index;
-//            expr += x[i][j];
-//        }
-//        model.addConstr(expr, GRB_EQUAL, 1);
-//        expr = 0;
-//    }
-//
-//    // Constraint 10
-//    for (int iIdx = 0; iIdx < nwd.size(); iIdx++) {
-//        GRBLinExpr expr;
-//        auto i = nwd[iIdx].Index;
-//        for (int jIdx = 0; jIdx < n; jIdx++) {
-//            auto j = nodes[jIdx].Index;
-//            expr += x[i][j];
-//        }
-//        model.addConstr(expr, GRB_EQUAL, 1);
-//        expr = 0;
-//    }
-//
-//    // Constraint 11
-//    GRBLinExpr constr11;
-//    for (int jIdx = 0; jIdx < nwd.size(); jIdx++) {
-//        auto j = nwd[jIdx].Index;
-//        constr11 += x[0][j];
-//    }
-//    model.addConstr(constr11, GRB_EQUAL, M);
-//    constr11 = 0;
-//
-//    // Constraint 12
-//    GRBLinExpr constr12;
-//    for (int jIdx = 0; jIdx < nwd.size(); jIdx++) {
-//        auto j = nwd[jIdx].Index;
-//        constr11 += x[j][0];
-//    }
-//    model.addConstr(constr11, GRB_EQUAL, M);
-//
-//    // Constraint 13
-//    for (int lIdx = 0; lIdx < nwd.size(); lIdx++) {
-//        auto l = nwd[lIdx].Index;
-//
-//    }
-//}
+tuple<int, int, double, bool, double> Solver::MulticommodityFlow(double tiLim) {
+    tuple<int, int, double, bool, double> result;
+    GRBEnv env;
+    GRBModel model(env);
+    auto nodes = instance.Nodes;
+    auto c = instance.DistanceMatrix;
+    auto n = instance.Dimension;
+    auto M = instance.NumberOfVehicles; // number of vehicles.
+    auto nss = instance.NodeSubsets; // subsets of nodes; contains at least 2 nodes; do not contain depots.
+    auto nwd = instance.NodesWithoutDepots;
+
+    auto** x = new GRBVar * [n];
+    for (int i = 0; i < n; i++) {
+        x[i] = reinterpret_cast<GRBVar *>(new GRBVar *[n]);
+        for (int j = 0; j < n; j++) {
+            x[i][j] = model.addVar(0, 1, 0.0, GRB_BINARY);
+        }
+    }
+
+    auto*** y = new GRBVar ** [n];
+    for (int i = 0; i < n; i++) {
+        y[i] = reinterpret_cast<GRBVar **>(new GRBVar ** [nwd.size()]);
+        for (int j = 0; j < nwd.size(); j++) {
+            y[i][j] = reinterpret_cast<GRBVar *>(new GRBVar * [nwd.size()]);
+            for (int l = 0; l < nwd.size(); l++) {
+
+            }
+        }
+    }
+    // Constraint 9
+    for (int jIdx = 0; jIdx < nwd.size(); jIdx++) {
+        GRBLinExpr expr;
+        auto j = nwd[jIdx].Index;
+        for (int iIdx = 0; iIdx < n; iIdx++) {
+            auto i = nodes[iIdx].Index;
+            expr += x[i][j];
+        }
+        model.addConstr(expr, GRB_EQUAL, 1);
+        expr = 0;
+    }
+
+    // Constraint 10
+    for (int iIdx = 0; iIdx < nwd.size(); iIdx++) {
+        GRBLinExpr expr;
+        auto i = nwd[iIdx].Index;
+        for (int jIdx = 0; jIdx < n; jIdx++) {
+            auto j = nodes[jIdx].Index;
+            expr += x[i][j];
+        }
+        model.addConstr(expr, GRB_EQUAL, 1);
+        expr = 0;
+    }
+
+    // Constraint 11
+    GRBLinExpr constr11;
+    for (int jIdx = 0; jIdx < nwd.size(); jIdx++) {
+        auto j = nwd[jIdx].Index;
+        constr11 += x[0][j];
+    }
+    model.addConstr(constr11, GRB_EQUAL, M);
+    constr11 = 0;
+
+    // Constraint 12
+    GRBLinExpr constr12;
+    for (int jIdx = 0; jIdx < nwd.size(); jIdx++) {
+        auto j = nwd[jIdx].Index;
+        constr11 += x[j][0];
+    }
+    model.addConstr(constr11, GRB_EQUAL, M);
+
+    // Constraint 13
+    for (int lIdx = 0; lIdx < nwd.size(); lIdx++) {
+        auto l = nwd[lIdx].Index;
+
+    }
+}
